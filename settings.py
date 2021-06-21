8b9b9ae11c7bc587a8e48e67b34d9e21962a850d
Karthik Chaoji <krozzgaming9@gmail.com>
2021-06-21 16:56:21 +0530

    Update settings.py
    Made the script dynamic

--- settings.py
+++ settings.py
@@ -1,62 +1,64 @@
 import all_functions
-
-option_message = '''----------------------
-[1] Change wallpaper
-[2] Save picture
-[3] Save picture as-
-[4] Settings
-[5] Help Message
-[0] Exit
-----------------------'''
-
-setting_message = '''----------------------
-:: SETTINGS ::
-[1] Change keywords
-[2] Change time limit
-[3] Change custom link
-[4] Change source of wallpapers
-[5] Option Message
-[0] Back
-----------------------'''
+from importlib import reload
 
 
-def setting_inputs():
+def options():
     while True:
-        print(':: OPTIONS ::')
-        print(option_message)
-        option = input('> ')
-        if option == '1':
+        # OPTIONS
+        option = all_functions.setting_function('Change wallpaper', 'Save picture',
+                                                'Save picture as-', 'Settings',
+                                                'Reset settings to default',
+                                                name='OPTIONS', updating=False,
+                                                auto_back_key=False, key='Exit')
+
+        if option == 1:
+            # Change wallpaper
             all_functions.run()
-        elif option == '2':
+        elif option == 2:
+            # Save picture
             all_functions.save_picture()
-        elif option == '3':
+        elif option == 3:
+            # Save picture as-
             all_functions.save_as()
-        elif option == '4':
+        elif option == 4:
+            # SETTINGS
             while True:
-                print(setting_message)
-                setting = input('> ')
-                if setting == '1':
+                setting = all_functions.setting_function('Change keywords', 'Change time delay',
+                                                         'Change custom link', 'Change source of wallpapers',
+                                                         'Change fit type', 'Quotes settings', name='SETTINGS',
+                                                         updating=False, auto_back_key=False)
+                if setting == 1:
+                    # Change keywords
                     all_functions.change_keywords()
-                elif setting == '2':
+                elif setting == 2:
+                    # Change time delay
                     all_functions.change_time()
-                elif setting == '3':
+                elif setting == 3:
+                    # Change custom link
                     all_functions.change_custom_link()
-                elif setting == '4':
-                    all_functions.change_source()
-                elif setting == '5':
-                    continue
-                elif setting == '0':
+                elif setting == 4:
+                    # Change source of wallpapers
+                    all_functions.setting_function('From Unsplash', 'From Wallpapers Folder',
+                                                   'From custom link', 'Quotes', 'Gradient colors', name='source')
+                elif setting == 5:
+                    # Change fit type
+                    all_functions.setting_function('Original size', 'Fit to screen', name='fit_type')
+                elif setting == 6:
+                    # Quotes settings
+                    all_functions.quote_settings()
+                elif setting == 0:
+                    options()
                     break
-        elif option == '5':
-            continue
-        elif option == '0':
-            print('Exiting')
+                reload(all_functions)
+                continue
+            break
+        elif option == 5:
+            all_functions.reset()
+        elif option == 0:
+            print('Exiting...')
             print('Okay see you!')
             break
-        else:
-            print('Please choose from the option indexes.')
-            continue
 
 
 if __name__ == '__main__':
-    setting_inputs()
+    options()
